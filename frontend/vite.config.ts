fcbe954825baca85c4843b25a439c7d452b23f48
keeee21 <keisuke.u211016@gmail.com>
2025-04-01 22:55:08 +0900

    feat: vite設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://file-zipper_api:3001',
-        changeOrigin: true,
-      }
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL || 'http://file-zipper_api:3001',
+          changeOrigin: true,
+        },
+      },
     },
   }
 })
